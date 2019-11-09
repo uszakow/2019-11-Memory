5acf24436793a84d907b89556cfca9643eeca601
Michal Stepien <michal.stepien36@gmail.com>
2019-11-09 13:20:51 +0100

    wyczyszczenie kodu

--- main.js
+++ main.js
@@ -1,41 +1,15 @@
 //1. Robimy tablicę z indeksami dla kart.
-const indexOfCard = [
-  0,
-  0,
-  1,
-  1,
-  2,
-  2,
-  3,
-  3,
-  4,
-  4,
-  5,
-  5,
-  6,
-  6,
-  7,
-  7,
-  8,
-  8,
-  9,
-  9
-];
+const indexOfCard = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 
 //stoper
-//2. Randomizujemy tablicę.
-const trialArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
 const licze = () => {
   let index = 0;
-  const timerSpan = document.getElementById('timer')
-    setInterval(() => timerSpan.innerText++, 1000);
+  const timerSpan = document.getElementById("timer");
+  setInterval(() => timerSpan.innerText++, 1000);
 };
 
 //2. Randomizujemy tablicę.
 shuffle(indexOfCard);
-
 function shuffle(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
@@ -53,22 +27,12 @@ getButton.addEventListener("click", function() {
   licze();
 });
 
-//setTimeout(() => clearInterval(myInterval), 10000)
-
-  shuffle(indexOfCard);
-  console.log(indexOfCard);
-});
-  shuffle(trialArray);
-  console.log(trialArray);
-
 //3. Pobieramy karty. Nadajemy zrandomizowane indeksy kartom.
 const cards = document.querySelectorAll(".card");
 cards.forEach(function(item, index) {
   item.setAttribute("data-index", indexOfCard[index]);
 });
 
-console.log(indexOfCard);
 //4. Przy nacisku na kartę zmieniamy tło w zależności od znaczenia 'data-index'. Zapisujemy, że to pierwsza otwarta karta
 
-
-//5. 
+//5.
