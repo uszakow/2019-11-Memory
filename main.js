28e62a53e12c0cbf696dd91ac387c3ad70bf12dc
uszakow <51965778+uszakow@users.noreply.github.com>
2019-11-09 12:03:08 +0100

    Indeksy dla divów

---
+++ main.js
@@ -0,0 +1,6 @@
+const indexOfCard = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(function (item, index) {
+    item.setAttribute('data-index', indexOfCard[index]);
+});
