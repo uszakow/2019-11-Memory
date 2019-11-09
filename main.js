32769e73973b553972bcf3642cd24b936cef79f1
Michal Stepien <michal.stepien36@gmail.com>
2019-11-09 13:37:34 +0100

    wstęp do moves

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 //1. Robimy tablicę z indeksami dla kart.
-const indexOfCard = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+const indexOfCard = [
+  0,
+  0,
+  1,
+  1,
+  2,
+  2,
+  3,
+  3,
+  4,
+  4,
+  5,
+  5,
+  6,
+  6,
+  7,
+  7,
+  8,
+  8,
+  9,
+  9
+];
 
 //2. Randomizujemy tablicę.
 const trialArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -14,14 +35,14 @@ const licze = () => {
 //2. Randomizujemy tablicę.
 shuffle(indexOfCard);
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 //
@@ -32,21 +53,29 @@ getButton.addEventListener("click", function() {
 
 //3. Pobieramy karty. Nadajemy zrandomizowane indeksy kartom.
 const cards = document.querySelectorAll(".card");
-cards.forEach(function (item, index) {
-    item.setAttribute("data-index", indexOfCard[index]);
+cards.forEach(function(item, index) {
+  item.setAttribute("data-index", indexOfCard[index]);
 });
 
 //4. Przy nacisku na kartę zmieniamy tło w zależności od znaczenia 'data-index'. Zapisujemy, że to pierwsza otwarta karta
+
+let zmienna = 0;
 function changeBackground() {
-    console.log(this);
-    // this.style.backgroundImage = 'url:(til)';
+  console.log(this);
+  // this.style.backgroundImage = 'url:(til)';
+  zmienna++;
+  console.log(zmienna);
+  if (zmienna == 2) {
+    zmienna = 0;
+    // console.log("mamy parę");
+  }
 }
 
 // cards.addEventListener('click', changeBackground);
 // console.log(cards);
 // document.querySelector('.card').addEventListener('click', changeBackground);
-cards.forEach(function (item) {
-    item.addEventListener('click', changeBackground);
-})
+cards.forEach(function(item) {
+  item.addEventListener("click", changeBackground);
+});
 
-//5. 
+//5.
