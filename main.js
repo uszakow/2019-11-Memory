9f5bfc9596a49d86444b6842afccb47ccbea8432
uszakow <51965778+uszakow@users.noreply.github.com>
2019-11-09 12:23:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-//1. Robimy tablicę z indeksami dla divów
+//1. Robimy tablicę z indeksami dla kart.
 const indexOfCard = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 
-//2. Randomizujemy tablicę
+//2. Randomizujemy tablicę.
 
 
-//3.
+//3. Pobieramy karty. Nadajemy zrandomizowane indeksy kartom.
 const cards = document.querySelectorAll('.card');
 cards.forEach(function (item, index) {
     item.setAttribute('data-index', indexOfCard[index]);
