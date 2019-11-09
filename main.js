3efe8cd62e005109a6635bc7c57e41c4313ddc8b
uszakow <51965778+uszakow@users.noreply.github.com>
2019-11-09 14:39:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,53 +3,55 @@ const indexOfCard = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
 
 //stoper
 const licze = () => {
-    let index = 0;
-    const timerSpan = document.getElementById("timer");
-    setInterval(() => timerSpan.innerText++, 1000);
+  let index = 0;
+  const timerSpan = document.getElementById("timer");
+  setInterval(() => timerSpan.innerText++, 1000);
 };
 
 //2. Randomizujemy tablicę.
 shuffle(indexOfCard);
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 //
 const getButton = document.getElementById("reset");
 getButton.addEventListener("click", function () {
-    licze();
+  licze();
 });
 
 //3. Pobieramy karty. Nadajemy zrandomizowane indeksy kartom.
 const cards = document.querySelectorAll(".card");
 cards.forEach(function (item, index) {
-    item.setAttribute("data-index", indexOfCard[index]);
+  item.setAttribute("data-index", indexOfCard[index]);
 });
 
 //4. Przy nacisku na kartę zmieniamy tło w zależności od znaczenia 'data-index'. Zapisujemy, że to pierwsza otwarta karta
 
 let zmienna = 0;
+let cardFirst,
+  cardSecond;
 function changeBackground() {
-    prevIndex = 0;
-    this.style.backgroundImage = (`url('tile-images/tile_${this.getAttribute('data-index')}.png')`);
+  prevIndex = 0;
+  this.style.backgroundImage = (`url('tile-images/tile_${this.getAttribute('data-index')}.png')`);
 }
 cards.forEach(function (item) {
-    item.addEventListener('click', changeBackground);
+  item.addEventListener('click', changeBackground);
 })
 
 zmienna++;
 console.log(zmienna);
 if (zmienna == 2) {
-    zmienna = 0;
-    const movesSpan = document.getElementById('moves')
-    +movesSpan.innerText++;
+  zmienna = 0;
+  const movesSpan = document.getElementById('moves')
+    + movesSpan.innerText++;
 }
 
 //5. Porównanie dwóch obrazków
