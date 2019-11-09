525c24df3a64b7abd2cfa4cbbcf74729f53bb2fb
uszakow <51965778+uszakow@users.noreply.github.com>
2019-11-09 16:02:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -126,20 +126,3 @@ cards.forEach(function(item) {
 //5. Porównujemy dwie karty
 
 //6. Obraca karty po 3 sec
-let cardFirst = null;
-let cardSecond = null;
-cards.forEach(function(item) {
-  item.addEventListener("click", backStartView);
-}, 3000);
-
-function backStartView() {
-  if (cardFirst === null) {
-    cardFirst = this;
-  } else {
-    cardSecond = this;
-  }
-  // console.log(cardFirst);
-  // console.log(cardSecond);
-  if (cardFirst !== null && cardSecond !== null) {
-  }
-}
